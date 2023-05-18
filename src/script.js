16a0140d87b92b96801a7723e6c5b29b772ac6d3
Asilvazavala <asilvazavala@gmail.com>
2023-05-18 10:11:38 -0600

    modified images size

--- src/script.js
+++ src/script.js
@@ -184,3 +184,15 @@ window.addEventListener('scroll', function() {
       message.value = ""
     }
   }
+
+/*   Descargar todas las imágenes de una página web
+  $$('img').forEach(img => {
+    const src = img.src
+    const [filename] = src.split('.').reverse()
+    const [name] = filename.split('.')
+
+    const a = document.createElement('a')
+    a.setAttribute('href', src)
+    a.setAttribute('downoload', name)
+    a.click()
+  }) */
