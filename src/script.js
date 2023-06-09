5ab98cd97d5a86e6a4405e56e73291ecabdd6b3b
Asilvazavala <asilvazavala@gmail.com>
2023-06-08 18:49:03 -0600

    added effect navbar

--- src/script.js
+++ src/script.js
@@ -172,6 +172,28 @@ window.addEventListener('scroll', function() {
     }
   }
 
+  // Hover li items Navbar
+  const listItem = document.querySelectorAll('nav ul li');
+  const menuBackDrop = document.querySelector('#menu-backdrop');
+
+  listItem.forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      const { left, top, width, height } = item.getBoundingClientRect()
+            
+      menuBackDrop.style.setProperty('--left', `${left}px`)
+      menuBackDrop.style.setProperty('--top', `${top}px`)
+      menuBackDrop.style.setProperty('--width', `${width}px`)
+      menuBackDrop.style.setProperty('--height', `${height}px`)
+      menuBackDrop.style.opacity = '1'
+      menuBackDrop.style.visibility = 'visible'
+    })
+
+    item.addEventListener('mouseleave', () => {
+      menuBackDrop.style.opacity = '0'
+      menuBackDrop.style.visibility = 'hidden'
+    })
+  })
+
 /*   Descargar todas las imágenes de una página web
   $$('img').forEach(img => {
     const src = img.src
