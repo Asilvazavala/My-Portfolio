4d51528b9ddfa78c1f7dac7d5761cb77b702e17d
Asilvazavala <asilvazavala@gmail.com>
2023-11-30 12:53:28 -0600

    modified section experience responsive

--- src/script.js
+++ src/script.js
@@ -16,6 +16,7 @@ const listItem = document.querySelectorAll('nav ul li');
 const menuBackDrop = document.querySelector('#menu-backdrop');
 const btnCV = document.getElementById('btnCV');
 const navbar = document.querySelector('.navbar');
+const articleExperiences = document.querySelectorAll('.experience-article');
 
 const experiences = [
   {
@@ -89,67 +90,8 @@ const experiences = [
 const experiencesContainer = document.getElementById('experiencesContainer');
 let activeExperience = null;
 
-experiences.forEach((experience, index) => {
-  const article = document.createElement('article');
-  const contentContainer = document.createElement('div');
-  const informationContainer = document.createElement('div');
-  const separatorContainer = document.createElement('div');
-  const yearSpan = document.createElement('span');
-  const line = document.createElement('div');
-  const iconContainer = document.createElement('div');
-  const titleH2 = document.createElement('h2');
-  const descriptionP = document.createElement('p');
-  const separator = document.createElement('div');
-
-  titleH2.dataset.section = 'about'; 
-  titleH2.dataset.value = `title-experience-${index}`;
-
-  descriptionP.dataset.section = 'about'; 
-  descriptionP.dataset.value = `desciption-experience-${index}`;
-
-  article.classList.add('experience-article');
-  contentContainer.classList.add('content-container');
-  separatorContainer.classList.add('separator-container');
-  informationContainer.classList.add('information-container');
-  line.classList.add('vertical-line');
-  iconContainer.classList.add('experience-icon');
-  
-  yearSpan.classList.add('experience-year');
-  titleH2.classList.add('experience-title');
-  descriptionP.classList.add('experience-description');
-  separator.classList.add('separator');
-
-  yearSpan.innerText = experience.year;
-  iconContainer.insertAdjacentHTML('beforeend', experience.icon);
-  titleH2.innerText = experience.title;
-  descriptionP.innerText = experience.description;
-
-  if (index !== experiences.length - 1) {
-    separator.innerHTML = `<span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span> <span class="symbol-space">|</span>`;
-  }
-
-  line.appendChild(separator);
-  separatorContainer.appendChild(line);
-
-  informationContainer.appendChild(titleH2);
-  informationContainer.appendChild(descriptionP);
-
-  contentContainer.appendChild(yearSpan);
-  contentContainer.appendChild(separatorContainer);
-  contentContainer.appendChild(iconContainer);
-  contentContainer.appendChild(informationContainer);
-
-  article.appendChild(contentContainer);
-
-  article.addEventListener('mouseover', () => {
-    setActiveExperience(article);
-  });
-
-  experiencesContainer.appendChild(article);
-});
-
 setActiveExperience(experiencesContainer.firstElementChild);
-
+    
 function setActiveExperience(experience) {
   if (activeExperience) {
     activeExperience.classList.remove('active');
@@ -159,6 +101,13 @@ function setActiveExperience(experience) {
   activeExperience = experience;
 }
 
+articleExperiences.forEach((article) => {
+  article.addEventListener('mouseover', () => {
+    setActiveExperience(article);
+  });
+});
+
+// Efecto Navbar
 function addEffectNavbar() {
   navbar.classList.add('scrolling');
   navbar.style.background = 'rgba(0, 0, 0, 0.2)';
